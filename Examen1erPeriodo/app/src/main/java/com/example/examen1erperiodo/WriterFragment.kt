572c2344c53104793744b8e9ee53c3df68206eab
Víctor Manuel Nevárez Herrera <78771990+victormnevarezh@users.noreply.github.com>
2021-09-14 17:41:24 -0500

    Primer Commit Creación de Proyecto y vista de login

---
+++ Examen1erPeriodo/app/src/main/java/com/example/examen1erperiodo/WriterFragment.kt
@@ -0,0 +1,11 @@
+package com.example.examen1erperiodo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class WriterFragment : Fragment(R.layout.fragment_writer) {
+
+}
