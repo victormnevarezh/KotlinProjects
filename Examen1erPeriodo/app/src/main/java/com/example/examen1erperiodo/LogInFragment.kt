eab1893df5f00e3277f5b27d8bc31d0e7e80b35d
Víctor Manuel Nevárez Herrera <78771990+victormnevarezh@users.noreply.github.com>
2021-09-15 13:56:08 -0500

    Cambios en clase user y cambios en LogInFragment.kt

--- Examen1erPeriodo/app/src/main/java/com/example/examen1erperiodo/LogInFragment.kt
+++ Examen1erPeriodo/app/src/main/java/com/example/examen1erperiodo/LogInFragment.kt
@@ -1,6 +1,8 @@
 package com.example.examen1erperiodo
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +10,8 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
 
 class LogInFragment : Fragment(R.layout.fragment_log_in) {
 
@@ -34,9 +38,38 @@ class LogInFragment : Fragment(R.layout.fragment_log_in) {
         (activity as MainActivity?)?.savePreferences(listUsers)
 
         imgLogIn = view.findViewById(R.id.imgLogIn)
+        imgLogIn.setImageResource(R.drawable.avatar)
         editTextUser = view.findViewById(R.id.editTextUser)
         editTextPassword = view.findViewById(R.id.editTextPassword)
         btnLogIn = view.findViewById(R.id.btnLogIn)
-        
+
+
+
+        // TODO: 15/09/2021 setEvents
     }
+
+    private fun changedEditTextUser () {
+        editTextUser.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                //No usado
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                listUsers.forEach {
+                    if (p0 == it.username) {
+                        imgLogIn.setImageResource(it.image)
+                    }
+                    else {
+                        imgLogIn.setImageResource(R.drawable.avatar)
+                    }
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                //No usado
+            }
+
+        })
+    }
+
 }
--- Examen1erPeriodo/app/src/main/java/com/example/examen1erperiodo/User.kt
+++ Examen1erPeriodo/app/src/main/java/com/example/examen1erperiodo/User.kt
@@ -7,20 +7,23 @@ import kotlinx.parcelize.Parcelize
 @JsonClass (generateAdapter = true)
 @Parcelize
 class User (
+    var username: String,
+    var password: String,
     var name: String,
     var type: Boolean,
+    var image: Int,
     var articles: Array<Article>,
     var loged: Boolean = false
         ) : Parcelable {
 
     companion object {
         val users = arrayOf<User>(
-            User("Tulio Triviño", true, arrayOf(Article.publishedArticles.elementAt(0), Article.publishedArticles.elementAt(1))),
-            User("Juan Carlos Bodoque", true, arrayOf(Article.publishedArticles.elementAt(2), Article.publishedArticles.elementAt(3), Article.publishedArticles.elementAt(4))),
-            User("Ernesto Felipe Mario Hugo", true, arrayOf(Article.publishedArticles.elementAt(5))),
-            User("Guaripolo", false, arrayOf(Article.publishedArticles.elementAt(0), Article.publishedArticles.elementAt(1), Article.publishedArticles.elementAt(2))),
-            User("Joe Pino", false, arrayOf(Article.publishedArticles.elementAt(1), Article.publishedArticles.elementAt(2), Article.publishedArticles.elementAt(4))),
-            User("Juanín Juan Harry", false, arrayOf(Article.publishedArticles.elementAt(3), Article.publishedArticles.elementAt(4), Article.publishedArticles.elementAt(5)))
+            User("tulio", "yo", "Tulio Triviño", true, R.drawable.tulio, arrayOf(Article.publishedArticles.elementAt(0), Article.publishedArticles.elementAt(1))),
+            User("bodoque", "123", "Juan Carlos Bodoque", true, R.drawable.bodoque, arrayOf(Article.publishedArticles.elementAt(2), Article.publishedArticles.elementAt(3), Article.publishedArticles.elementAt(4))),
+            User("mariohugo", "123", "Ernesto Felipe Mario Hugo", true,  R.drawable.mariohugo, arrayOf(Article.publishedArticles.elementAt(5))),
+            User("guaripolo", "123", "Guaripolo", false, R.drawable.guaripolo, arrayOf(Article.publishedArticles.elementAt(0), Article.publishedArticles.elementAt(1), Article.publishedArticles.elementAt(2))),
+            User("joepino", "123", "Joe Pino", false, R.drawable.joepino, arrayOf(Article.publishedArticles.elementAt(1), Article.publishedArticles.elementAt(2), Article.publishedArticles.elementAt(4))),
+            User("juanin", "123", "Juanín Juan Harry", false, R.drawable.juanin, arrayOf(Article.publishedArticles.elementAt(3), Article.publishedArticles.elementAt(4), Article.publishedArticles.elementAt(5)))
         )
     }
 }
