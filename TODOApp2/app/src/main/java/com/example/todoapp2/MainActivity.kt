db6f07a8834f808f06f52fc4e7730d25fcce6fce
Víctor Manuel Nevárez Herrera <victor.m.nevarez.h@gmail.com>
2021-11-23 01:28:05 -0600

    Reparación de la wea del main
    Aún falta de reparar el coso de TaskDataBase porque por alguna razón no lo está reconociendo
    T.T

--- TODOApp2/app/src/main/java/com/example/todoapp2/MainActivity.kt
+++ TODOApp2/app/src/main/java/com/example/todoapp2/MainActivity.kt
@@ -1,35 +1,47 @@
 package com.example.todoapp2
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
+import androidx.work.Data
+import androidx.work.ExistingWorkPolicy
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+import com.example.todoapp2.database.TaskDatabase
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 import java.time.LocalDateTime
 import java.time.Month
+import java.time.OffsetDateTime
 import java.util.*
+import java.util.concurrent.TimeUnit
 
+@RequiresApi(Build.VERSION_CODES.O)
 class MainActivity : AppCompatActivity() {
 
     companion object {
         val NEW_TASK = 200
-        val NEW_TASK_KEY = "newTask"
+        val NEW_TASK_KEY = "new_task"
+        val UPDATE_TASK = 201
     }
+    private val TASKS = "tasks"
+    private var tasks = mutableListOf<Task>()
 
-    private lateinit var rcvTask: RecyclerView
-    private lateinit var btnAddTask: FloatingActionButton
-
-    //Key de estado
-    private val SAVED_TASKS = "tasks"
-
+    private lateinit var add: FloatingActionButton
+    private lateinit var rcv: RecyclerView
     private lateinit var adapter: TasksAdapter
-    private var tasks = mutableListOf(
-        Task(0, "Test", "description", LocalDateTime.now()),
-        Task(1, "Test1", "description1", LocalDateTime.of(2021, Month.NOVEMBER, 6, 12, 40)),
-        Task(2, "Test2", "description2", LocalDateTime.of(2022, Month.NOVEMBER, 6, 12, 40)),
-        Task(3, "Test3", "description3", LocalDateTime.of(2023, Month.NOVEMBER, 6, 12, 40)),
-    )
+
+    private lateinit var db: TaskDatabase
 
     //Mínimo necesario para una app
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,7 +49,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         savedInstanceState?.let {
-            val savedTasks = it.getParcelableArrayList<Task>(SAVED_TASKS)?.toMutableList() ?: tasks
+            val savedTasks = it.getParcelableArrayList<Task>(TASKS)?.toMutableList() ?: tasks
             tasks = savedTasks
         }
 
@@ -47,36 +59,114 @@ class MainActivity : AppCompatActivity() {
 
     //Inicializar vistas
     private fun initViews() {
-        rcvTask = findViewById(R.id.rcvTasks)
-        btnAddTask = findViewById(R.id.btnAddTask)
-
-        btnAddTask.setOnClickListener{
+        add = findViewById(R.id.add)
+        rcv = findViewById(R.id.rcvTasks)
+        add.setOnClickListener {
             startActivityForResult(Intent(this, FormActivity::class.java), NEW_TASK)
         }
     }
 
-    private fun setAdapter() {
-        adapter = TasksAdapter(tasks)
+    override fun onResume() {
+        super.onResume()
+        db = Room.databaseBuilder(this, TaskDatabase::class.java, "Tasks").build()
 
-        rcvTask.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        rcvTask.adapter = adapter
+        MainScope().launch {
+            tasks = db.taskDao().getPendingTasks().toMutableList()
+            setAdapter()
+        }
+    }
+
+
+    private fun setAdapter() {
+        adapter = TasksAdapter(
+            tasks,
+            onClickFinishedTask = {
+                    task, position ->
+                MainScope().launch {
+                    db.taskDao().updateTask(task.apply {
+                        status = false
+                    })
+                    adapter.removeTask(position)
+                    val workManager = WorkManager.getInstance(this@MainActivity)
+                    workManager.cancelUniqueWork("NOTIFICATION_WORK ${task.id}")
+                }
+            },
+            onClickDetailsTask = {
+                    task ->
+                startActivityForResult(Intent(this, FormActivity::class.java).apply {
+                    putExtra("isTaskDetail", true)
+                    putExtra("task", task)
+                }, UPDATE_TASK)
+            }
+        )
+        rcv.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        rcv.adapter = adapter
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-
         outState.apply {
-            putParcelableArrayList(SAVED_TASKS, tasks as ArrayList<Task>)
+            putParcelableArrayList(TASKS, tasks as ArrayList<Task>)
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == NEW_TASK) {
+        if ( requestCode == NEW_TASK ) {
             data?.getParcelableExtra<Task>(NEW_TASK_KEY)?.let {
-                adapter.addTask(it)
+                MainScope().launch(Dispatchers.Main){
+                    adapter.addTask(it)
+                }
+
+                MainScope().launch(Dispatchers.IO){
+                    val id = db.taskDao().saveNewTask(it)
+
+                    val zone = OffsetDateTime.now().offset
+                    val selectedMillis = it.Date?.toInstant(zone)?.toEpochMilli() ?:0
+                    val nowMillis = LocalDateTime.now().toInstant(zone).toEpochMilli()
+
+                    scheduleNotification(selectedMillis - nowMillis, Data.Builder().apply {
+                        putInt("notificationID", id.toInt())
+                        putString("notificationTitle", it.Title)
+                        putString("notificationDescription", it.Description)
+                        putString("notificationDate", it.Date.toString())
+                    }.build())
+                }
             }
+        } else {
+            data?.getParcelableExtra<Task>(NEW_TASK_KEY)?.let {
+                MainScope().launch(Dispatchers.Main) {
+                    adapter.updateTask(it)
+                }
+
+                MainScope().launch(Dispatchers.IO) {
+                    db.taskDao().updateTask(it)
+                }
+            }
+        }
+    }
+
+    private fun scheduleNotification(delay: Long, data: Data){
+        val notificationWork = OneTimeWorkRequest.Builder(com.example.todoapp2.notification.NotificationManager::class.java)
+            .setInitialDelay(delay, TimeUnit.MILLISECONDS).setInputData(data).build()
+        val instanceWorkManager = WorkManager.getInstance(this)
+        instanceWorkManager.beginUniqueWork(
+            "NOTIFICATION_WORK ${data.getInt("notificationID", 0)}",
+            ExistingWorkPolicy.APPEND_OR_REPLACE, notificationWork
+        ).enqueue()
+    }
+
+    private fun createNotificationChannel(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "TASK"
+            val descriptionText = "Channel of pending tasks"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel("TASK_CHANNEL", name, importance).apply {
+                description = descriptionText
+            }
+
+            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
         }
     }
 }
--- TODOApp2/app/src/main/java/com/example/todoapp2/notification/NotificationManager.kt
+++ TODOApp2/app/src/main/java/com/example/todoapp2/notification/NotificationManager.kt
@@ -1,4 +1,52 @@
 package com.example.todoapp2.notification
 
-class NotificationManager {
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.example.todoapp2.FormActivity
+import com.example.todoapp2.R
+import com.example.todoapp2.Task
+import java.time.LocalDateTime
+
+@RequiresApi(Build.VERSION_CODES.O)
+class NotificationManager(private val context: Context, params: WorkerParameters) : Worker(context, params) {
+    override fun doWork(): Result {
+        val id = inputData.getInt("notificationID", 0)
+        val title = inputData.getString("notificationTitle") ?: ""
+        val description = inputData.getString("notificationDescription") ?: ""
+        val date = LocalDateTime.parse(inputData.getString("notificationDate"))
+
+        createNotification(Task(id, title, description, date))
+
+        return Result.success()
+    }
+
+    fun createNotification(task: Task){
+        val intent: PendingIntent = PendingIntent.getActivity(
+            context,
+            1,
+            Intent(context, FormActivity::class.java).apply {
+                putExtra("isTaskDetail", true)
+                putExtra("task", task)
+            },
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val builder = NotificationCompat.Builder(context, "TASK_CHANNEL")
+            .setSmallIcon(R.drawable.ic_add)
+            .setContentTitle(task.Title)
+            .setContentText(task.Description)
+            .setPriority(NotificationCompat.PRIORITY_MAX)
+            .setContentIntent(intent)
+            .setAutoCancel(true)
+        with(NotificationManagerCompat.from(context)){
+            notify(task.id, builder.build())
+        }
+    }
 }
